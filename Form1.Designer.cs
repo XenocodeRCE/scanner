2e23e1cfe2e6ab345ef7657699e355c51ce7f4b8
Sam Harwood <support@netguard.io>
2019-04-09 03:17:17 +0200

    init

---
+++ Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace Scanner {
+    partial class Form1 {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent() {
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(-10, 78);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(271, 13);
+            this.panel3.TabIndex = 16;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(24, 54);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(163, 17);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Scan for entities to protect";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 17F);
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(57)))), ((int)(((byte)(146)))));
+            this.label4.Location = new System.Drawing.Point(23, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 31);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Scanner";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(57)))), ((int)(((byte)(146)))));
+            this.panel2.Location = new System.Drawing.Point(261, -7);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(4, 308);
+            this.panel2.TabIndex = 15;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.ItemSize = new System.Drawing.Size(58, 5);
+            this.tabControl1.Location = new System.Drawing.Point(-6, 81);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(273, 224);
+            this.tabControl1.TabIndex = 11;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.SystemColors.Control;
+            this.tabPage1.Controls.Add(this.panel1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 9);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(265, 211);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            // 
+            // panel1
+            // 
+            this.panel1.AllowDrop = true;
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(6, 6);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(251, 199);
+            this.panel1.TabIndex = 0;
+            this.panel1.DragDrop += new System.Windows.Forms.DragEventHandler(this.panel1_DragDrop);
+            this.panel1.DragEnter += new System.Windows.Forms.DragEventHandler(this.panel1_DragEnter);
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Enabled = false;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(64, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 17);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Drop your file here";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.richTextBox1.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(271, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(249, 299);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(520, 299);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.tabControl1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "NETLIcense.IO -Scanner";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,192 @@
+﻿using dnlib.DotNet;
+using dnlib.DotNet.Emit;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Scanner {
+    public partial class Form1 : Form {
+
+        public string DirectoryName = "";
+        public ModuleDefMD module;
+        public Dictionary<Instruction, int> toFeedS;
+        public Dictionary<MethodDef, int> toFeedM;
+
+        public Form1() {
+            InitializeComponent();
+            toFeedS = new Dictionary<Instruction, int>();
+            toFeedM = new Dictionary<MethodDef, int>();
+        }
+
+        private void panel1_DragEnter(object sender, DragEventArgs e) {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) {
+                e.Effect = DragDropEffects.Copy;
+            } else {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+
+        private void panel1_DragDrop(object sender, DragEventArgs e) {
+            try {
+                Array array = (Array)e.Data.GetData(DataFormats.FileDrop);
+                if (array != null) {
+                    string text = array.GetValue(0).ToString();
+                    int num = text.LastIndexOf(".", StringComparison.Ordinal);
+                    if (num != -1) {
+                        string text2 = text.Substring(num);
+                        text2 = text2.ToLower();
+                        if (text2 == ".exe" || text2 == ".dll") {
+                            Activate();
+                            module = ModuleDefMD.Load(text);
+                            richTextBox1.Clear();
+                            richTextBox1.AppendText($"[+] File loaded : {module.Name}", Color.Black, true);
+
+                            foreach (var t in module.GetTypes(true)) {
+                                richTextBox1.AppendText($"----------------------------\n");
+                                richTextBox1.AppendText($"[-]Checking type : {t.Name}\n");
+                                foreach (var m in t.Methods(true)) {
+                                    if (HasInCompatibleParameters(m))
+                                        continue;
+                                    if (HasReferencesToFields(m))
+                                        continue;
+                                    if (HasExternalCall(m))
+                                        continue;
+                                    //toFeedM.Add(m, 5);
+                                    richTextBox1.AppendText($"[+]Compatible method : {m.Name}", Color.Blue, true);
+                                }
+
+                                foreach (var f in t.Fields) {
+                                    if (!f.FieldType.IsCorLibType)
+                                        continue;
+                                    richTextBox1.AppendText($"[+]Compatible field : {f.Name}", Color.CadetBlue, true);
+                                }
+                            }
+
+
+                            int num2 = text.LastIndexOf("\\", StringComparison.Ordinal);
+                            if (num2 != -1) {
+                                DirectoryName = text.Remove(num2, text.Length - num2);
+                            }
+                            if (DirectoryName.Length == 2) {
+                                DirectoryName += "\\";
+                            }
+                        }
+                    }
+                }
+            } catch (Exception ex){
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        public bool HasExternalCall(MethodDef m) {
+            var instr = m.Body.Instructions;
+            foreach (var item in instr) {
+                if (item.OpCode == OpCodes.Call && item.Operand is IMethod) {
+                    var method = (IMethod)item.Operand;
+                    if (!method.DeclaringType.DefinitionAssembly.IsCorLib()) {
+                        toFeedM.Add(m, 0);
+                        return true;
+                    }
+                }
+
+                if (item.OpCode == OpCodes.Newobj && item.Operand is IMethod) {
+                    var method = (IMethod)item.Operand;
+                    if (!method.DeclaringType.DefinitionAssembly.IsCorLib()) {
+                        toFeedM.Add(m, 0);
+                        return true;
+                    }
+                }
+
+                if (item.OpCode == OpCodes.Ldtoken && item.Operand is IMethod) {
+                    var method = (IMethod)item.Operand;
+                    if (!method.DeclaringType.DefinitionAssembly.IsCorLib()) {
+                        toFeedM.Add(m, 0);
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public bool HasReferencesToFields(MethodDef m) {
+
+            var instr = m.Body.Instructions;
+            foreach (var item in instr) {
+                if (item.OpCode == OpCodes.Ldfld) {
+                    toFeedM.Add(m, 0);
+                    return true;
+                }
+                if (item.OpCode == OpCodes.Ldflda) {
+                    toFeedM.Add(m, 0);
+                    return true;
+                }
+                if (item.OpCode == OpCodes.Stfld) {
+                    toFeedM.Add(m, 0);
+                    return true;
+                }
+                if (item.OpCode == OpCodes.Stsfld) {
+                    toFeedM.Add(m, 0);
+                    return true;
+                }
+                if (item.OpCode == OpCodes.Ldsfld) {
+                    IField f = (IField)item.Operand;
+                    if (!f.FieldSig.Type.DefinitionAssembly.IsCorLib()) {
+                        toFeedM.Add(m, 0);
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public bool HasInCompatibleParameters(MethodDef m) {
+            if(m.Parameters.Count == 0) {
+                toFeedM.Add(m, 0);
+                return true;
+            }
+            foreach (var p in m.Parameters) {
+                if (!p.Type.IsCorLibType) {
+                    toFeedM.Add(m, 0);
+                    return true;
+                }
+            }
+            if (!m.HasReturnType) {
+                toFeedM.Add(m, 0);
+                return true;
+            }
+            return false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            richTextBox1.AppendText("Hello and Welcome to NETLICense.IO Scanner. \n");
+            richTextBox1.AppendText("To start, please drag and drop a file \n");
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e) {
+
+        }
+
+       
+    }
+
+    public static class RichTextBoxExtensions {
+        public static void AppendText(this RichTextBox box, string text, Color color, bool v) {
+            if(v)
+                text += Environment.NewLine;
+
+            box.SelectionStart = box.TextLength;
+            box.SelectionLength = 0;
+
+            box.SelectionColor = color;
+            box.AppendText(text);
+            box.SelectionColor = box.ForeColor;
+        }
+    }
+
+}
